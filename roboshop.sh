8f2fc6a41306bf9d6c791dd03611bd3347fb4788
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-30 17:39:48 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,9 @@
 SG_ID="sg-008da60b2f8f5f062"
 AMI_ID="ami-0220d79f3f480ecf5"
 
+ZONE_ID="Z00271701JDWNDD0HCY5L"
+DOMAIN_NAME="annuru.online"
+
 for INSTANCE in $@
 do
 
@@ -16,20 +19,49 @@ INSTANCE_ID=$(aws ec2 run-instances \
     )
 
 if [ $INSTANCE == "frontend" ]; then
-   IP=$(aws ec2 describe-instances 
+   IP=$(aws ec2 escribe-instances 
     --instance-ids $INSTANCE_ID \
     --query 'Reservations[].Instances[].PublicIpAddress' \
     --output text
    )
+   RECORD_NAME="$DOMAIN_NAME"
+
 else
    IP=$(aws ec2 describe-instances \
     --instance-ids $INSTANCE_ID \
     --query 'Reservations[].Instances[].PrivateIpAddress' \
     --output text
    )
+   RECORD_NAME="$INSTANCE.$DOMAIN_NAME"
 
 fi
 
 echo "IP Address : $IP"
+
+aws route53 change-resource-record-sets \
+--hosted-zone-id $ZONE_ID \
+ --change-batch '
+    {
+        "Comment": "Update records",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+
+
+
+'
 
 done
