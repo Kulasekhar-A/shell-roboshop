e5ed8f28aa7ec779590d7a596ead6de7b640c22b
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-30 21:14:03 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -14,7 +14,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
     --instance-type t3.micro \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
-    --query 'Instances[0].InstanceId' \
+    --query 'Instances[0].InstanceId' 
     --output text
     )
 
