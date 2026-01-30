304721609495aa8249cddddae3c5022ea00e4534
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-30 22:43:15 +0530

    actions

---
+++ redis.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+  echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+  exit 1
+fi
+
+mkdir -p  $LOGS_FOLDER
+
+VALIDATE(){
+   if [ $1 -ne 0 ]; then
+       echo -e" $2 ...$R FAILURE  $N" | tee -a $LOGS_FILE
+       exit 1
+   else
+       echo -e " $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+   fi
+}
+
+dnf module disable redis -y &>> $LOGS_FILE
+VALIDATE $? "Disabled redis default version"
+
+dnf module enable redis:7 -y
+VALIDATE $? "Enable redis current version"
+
+dnf install redis -y 
+VALIDATE $? "Installing Redis ..."
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
+
+sed -i 's/yes/no/g' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
