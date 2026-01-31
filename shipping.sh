a038a2e24fc9fa91f582dfe1f1ca8e7761ecd866
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-31 08:12:03 +0530

    actions

--- shipping.sh
+++ shipping.sh
@@ -8,7 +8,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
+SCRIP_DIR=$PWD
+MYSQL_HOST=mysql.annuru.online
 
 USERID=$(id -u)
 
@@ -60,3 +61,31 @@ VALIDATE $? "Clean the default package"
 
 mv target/shipping-1.0.jar shipping.jar &>> $LOGS_FILE
 VALIDATE $? "Renaming jar file"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>> $LOGS_FILE
+VALIDATE $? "Copying cart service"
+
+systemctl daemon-reload &>> $LOGS_FILE
+VALIDATE $? "Reload the service"
+
+systemctl enable shipping &>> $LOGS_FILE
+VALIDATE $? "Enable the shipping service"
+
+systemctl start shipping &>> $LOGS_FILE
+VALIDATE $? "Start the shipping service"
+
+dnf install mysql -y  &>> $LOGS_FILE
+VALIDATE $? "Install mysql"
+
+mysql -h $MYSQL_HOST -uroot -pRoboshop@1 -e "USE cities;" &>> $LOGS_FILE
+if [ $? -ne 0 ]; then
+  echo "Loading shipping schema"
+  mysql -h $MYSQL_HOST -uroot -pRoboshop@1 < /app/db/schema.sql &>> $LOGS_FILE
+  mysql -h $MYSQL_HOST -uroot -pRoboshop@1 < /app/db/app-user.sql &>> $LOGS_FILE
+  mysql -h $MYSQL_HOST -uroot -pRoboshop@1 < /app/db/master-data.sql &>> $LOGS_FILE
+else
+  echo "Database already exists, skipping schema load"
+fi
+
+systemctl restart shipping &>> $LOGS_FILE
+VALIDATE $? "Restart the shipping service"
