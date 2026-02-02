498aa43f83a5568e0b2ffe252d35b43b4be7d553
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-02 14:30:16 +0530

    actions

--- mysql.sh
+++ mysql.sh
@@ -19,7 +19,7 @@ mkdir -p  $LOGS_FOLDER
 
 VALIDATE(){
    if [ $1 -ne 0 ]; then
-       echo -e " $2 ...$R FAILURE  $N" | tee -a $LOGS_FIL
+       echo -e " $2 ...$R FAILURE  $N" | tee -a $LOGS_FILE
        exit 1
    else
        echo -e " $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
