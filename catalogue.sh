030cd483f51c31987543e1955bea2407bc92d6d5
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-02 13:59:48 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -22,7 +22,7 @@ mkdir -p  $LOGS_FOLDER
 
 VALIDATE(){
    if [ $1 -ne 0 ]; then
-       echo -e " $2 ...$R FAILURE  $N" | tee -a $LOGS_FILE
+       echo -e " $2 ...$R FAILURE  $N" | tee -a $LOGS_FIL
        exit 1
    else
        echo -e " $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
